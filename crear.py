7f58680aac410f96266d6f9ab2a99a7981221d45
Javier Neri <theneri@gmail.com>
2015-03-30 16:13:22 -0500

    comentarios de codigo
    comentarios de codigo

--- crear.py
+++ crear.py
@@ -1,27 +1,27 @@
 import xlsxwriter
 
 
-# Create an new Excel file and add a worksheet.
+# crear un nuevo archivo y una hoja de trabajo.
 workbook = xlsxwriter.Workbook('demo.xlsx')
 worksheet = workbook.add_worksheet()
 
-# Widen the first column to make the text clearer.
+# hacemos mas grande la columa A 
 worksheet.set_column('A:A', 20)
 
-# Add a bold format to use to highlight cells.
+# agregamos a la variable bold atributos de formato
 bold = workbook.add_format({'bold': True})
 
-# Write some simple text.
+# agregamos texto simple
 worksheet.write('A1', 'Hello')
 
-# Text with formatting.
+# agreganmos texto con formato
 worksheet.write('A2', 'World', bold)
 
-# Write some numbers, with row/column notation.
+# escribir datos usando numeros para fila y columnas
 worksheet.write(2, 0, 123)
 worksheet.write(3, 0, 123.456)
 
-# Insert an image.
-worksheet.insert_image('B5', 'logo.png')
+# insertar una imagen
+#worksheet.insert_image('B5', 'logo.png')
 
 workbook.close()
